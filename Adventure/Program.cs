e71a90674edbbe66a6cd5da8f875aaa9cbbc0d75
CodySalerno <114028734+CodySalerno@users.noreply.github.com>
2022-09-23 11:52:10 -0500

    Fixed issues with attack function return statements triggering too soon and added game overs

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -1,16 +1,34 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
 namespace Adventure
 {
     class Program
     {
+        public bool isAlive = true;
         static void Main()
         {
-            Character[] Tutorial_Players = Encounters.Tutorial();
-            Character[] Cave_players = Encounters.Cave(Tutorial_Players);
+            Character Player = Encounters.Tutorial();
+            Character Cave_Players = Encounters.Cave(Player);
+        }
+        public static void GameOver(int reason=0)
+        {
+            switch (reason)
+            {
+                case 0:
+                    Console.WriteLine("Game Over, you have died.");
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Console.WriteLine("Congratulations, you've slain the dragon and beaten the game.");
+                    Environment.Exit(0);
+                    break;
+            }
         }
     }
-    class Character {
-        int Health;
+    class Character
+    {
+        public int Health;
         int HealthMax;
         int Strength;
         int Speed;
@@ -25,6 +43,10 @@ namespace Adventure
             this.Name = name;
             this.Inventory = inventory;
         }
+        internal void Check_stats()
+        {
+            Console.WriteLine("Health: " + this.Health + "/" + this.HealthMax + " Strength: " + this.Strength + " Speed: " + this.Speed);
+        }
         internal void Add_Inventory(string item, int value)
         {
             this.Inventory.AddToInv(item, value);
@@ -32,7 +54,8 @@ namespace Adventure
         internal void Check_Inventory()
         {
             bool emptyInv = this.Inventory.Check_Inventory();
-            if (emptyInv) {
+            if (emptyInv)
+            {
                 Console.WriteLine(this.Name + " has an empty inventory");
             }
         }
@@ -47,20 +70,21 @@ namespace Adventure
         {
             this.Health = this.HealthMax;
         }
-        bool Attack(Character opponent) //returns if opponent dies
+        bool Attack(Character opponent) //returns if opponent is still alive
         {
             Console.WriteLine(this.Name + " attacks " + opponent.Name + " dealing " + this.Strength + " damage.");
             opponent.Health -= this.Strength; //deals damage
-            if (opponent.Health <= 0) //checks if opponent dead
+            if (opponent.Health > 0) //checks if opponent dead
             {
-                opponent.Health = 0;
-                Console.WriteLine(opponent.Name + " has succumbed to its injuries.");
                 return true;
             }
+            Console.WriteLine(opponent.Name + " has succumbed to its injuries.");
+            opponent.Health = 0;
             return false;
         }
         internal bool Fight(Character opponent)
         {
+            bool both_alive = true;
             bool Self_First; //decides who should attack this or opponent
             if (this.Speed == opponent.Speed) //chooses random start
             {
@@ -71,7 +95,7 @@ namespace Adventure
                 }
                 else
                 {
-                    Self_First= false;
+                    Self_First = false;
                 }
             }
             else if (this.Speed > opponent.Speed) //this attacks first because it's faster
@@ -82,21 +106,22 @@ namespace Adventure
             {
                 Self_First = false;
             }
-            while (this.Health > 0 && opponent.Health > 0) //opponent attacks first because it's faster
+            while (both_alive) //opponent attacks first because it's faster
             {
-               if (Self_First)
+                if (Self_First)
                 {
-                    Attack(opponent);
+                    both_alive = Attack(opponent);
                     Self_First = false;
                 }
-               else
+                else
                 {
-                    opponent.Attack(this);
+                    both_alive = opponent.Attack(this);
                     Self_First = true;
                 }
-                if (this.Health == 0) return false;
-                else return true;
+                
             }
+            if (this.Health == 0) return false;
+            else return true;
             return true; //shouldn't ever trigger but if it does means both people had 0 hp at the start of the fight
         }
     }
@@ -133,23 +158,70 @@ namespace Adventure
             return false;
         }
     }
-    class Encounters 
+    class Encounters
     {
-        public static Character[] Tutorial()
+        public static Character Tutorial()
         {
             Console.WriteLine("What is your name");
-            Character Player = new(new int[] {5,5,5}, Console.ReadLine(), new Inventory());
-            Character Spider = new(new int[] {3,3,3}, "Spider", new Inventory());
+            Character Player = new(new int[] { 3, 3, 4 }, Console.ReadLine(), new Inventory());
+            Character Spider = new(new int[] { 1, 1, 1 }, "Spider", new Inventory());
             Player.Fight(Spider);
-            Player.Level_Up(1,1,1);
-            return new Character[] {Player, Spider};
+            Player.Check_stats();
+            Player.Level_Up(1, 1, 1);
+            Player.Check_stats();
+            return Player;
         }
-        public static Character[] Cave(Character[] Inputs)
+        public static Character Cave(Character input)
         {
-            Character Player = Inputs[0];
+            Character Player = input; //Player character
             Console.WriteLine("You enter a dark cave");
-            return new Character[] {Player};
+            Console.WriteLine("There's a sleeping dragon to your right, and a door to your left which would you like to approach?");
+            string response = Console.ReadLine().ToLower();
+            if (response.Contains("dragon"))
+            {
+                Encounters.Dragon(Player);
+            }
+            else if (response.Contains("door"))
+            {
+                Encounters.Door(Player);
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, I don't understand");
+                Cave(input);
+            }
+            return Player;
         }
-
+        public static Character Dragon(Character input)
+        {
+            Character Player = input;
+            Console.WriteLine("You approach the dragon and it raises its head and prepares to attack.");
+            Character dragon = new(new int[] { 5, 5, 3 }, "Dragon", new Inventory());
+            dragon.Check_stats();
+            Player.Fight(dragon);
+            Player.Check_stats();
+            if (Player.Health == 0)
+            {
+                Program.GameOver(); //defeat
+            }
+            if (dragon.Health == 0)
+            {
+                Program.GameOver(1); //game won
+            }
+            return Player;
+        }
+        public static Character Door(Character input)
+        {
+            Character Player = input;
+            Console.WriteLine("You find a treasure chest with a sword inside, do you take it?");
+            string response = Console.ReadLine().ToLower();
+            if (response.Contains('y'))
+            {
+                Player.Level_Up(0, 3, -1);
+                Player.Check_stats();
+            }
+            Encounters.Dragon(Player);
+            return Player;
+        }
     }
 }
